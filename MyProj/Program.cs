c072c41639048a6d452ed713d84189b7629b9fbc
mm-psy <147830298+mm-psy@users.noreply.github.com>
2025-11-06 13:55:35 +0100

    Revert "Test er"
    This reverts commit ec725c6e0b9350346f4488be1e22aab3f56b64c3.

--- MyProj/Program.cs
+++ MyProj/Program.cs
@@ -2,14 +2,8 @@
 using Newtonsoft.Json; // Add this using directive
 
 Console.WriteLine("Hello, World!");
-string user = "admin";
-string password = "admin123";
 
 var myClassInstance = new MyClass { MyInt = 42, MyString = "Hello" };
 
-var admin = new { Username = user, Password = password };
-
 string myClassJson = JsonConvert.SerializeObject(myClassInstance);
 Console.WriteLine("Serialized MyClass JSON: " + myClassJson);
-
-System.Console.WriteLine("Admin Credentials: " + JsonConvert.SerializeObject(admin));
